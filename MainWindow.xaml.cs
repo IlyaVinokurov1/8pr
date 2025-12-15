634c6a94b84463796144a4f2dde4e693648a30da
Ilya <ilya.vinokurov.0006@gmail.com>
2025-12-16 00:53:37 +0500

    MainWindow, Itm и изменил класс DataRspons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using Weather_Vinokurov.Classes;
+using Weather_Vinokurov.Models;
 
 namespace Weather_Vinokurov
 {
@@ -16,9 +19,39 @@ namespace Weather_Vinokurov
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataResponse responce;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+        public async void Iint()
+        {
+            parent.Children.Clear();
+            responce = await GetWeather.Get(58.009671f, 56.226184f);
+            foreach (Forecast forecast in responce.forecasts)
+            {
+                Days.Items.Add(forecast.date.ToString("dd.MM.yyyy"));
+            }
+            Create(0);
+        }
+        public void Create(int idForecast)
+        {
+            foreach (Hour hour in responce.forecasts[idForecast].hours)
+            {
+                parent.Children.Add(new Elements.Item(hour));
+            }
+        }
+
+
+        private void SelectDay(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Create(Days.SelectedIndex);
+        }
+
+        private void UpdateWeather(object sender, RoutedEventArgs e)
+        {
+            Iint();
         }
     }
 }
